ea9ee3699a0943404afa8e0104e22b0dd6459c9a
JeremyParadie <jparadie@gmail.com>
2024-08-26 23:59:13 -0400

    Quartz Publish Button initial commit

--- main.ts
+++ main.ts
@@ -1,134 +1,108 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting, Notice, normalizePath } from 'obsidian';
+import * as fs from 'fs';
 
-// Remember to rename these classes and interfaces!
+const { exec } = require('child_process');
 
-interface MyPluginSettings {
-	mySetting: string;
+interface QuartzPublishButtonPluginSettings {
+    quartzPath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: QuartzPublishButtonPluginSettings = {
+    quartzPath: 'default'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class QuartzPublishButtonPlugin extends Plugin {
+    settings: QuartzPublishButtonPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+    async onload() {
+        await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+        // Create a button in the ribbon (left sidebar)
+        const ribbonIconEl = this.addRibbonIcon('rocket', 'Sync to Quartz', () => {
+            this.runQuartzSync();
+        });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+        ribbonIconEl.addClass('quartz-publish-button-ribbon');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+        // Add commands to Obsidian's command palette
+        this.addCommand({
+            id: 'run-quartz-sync',
+            name: 'Run Quartz Sync',
+            callback: () => this.runQuartzSync()
+        });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        // Add the settings tab to configure the plugin
+        this.addSettingTab(new QuartzSettingTab(this.app, this));
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    // Function to validate Quartz path
+    validateQuartzPath(): boolean {
+        if (this.settings.quartzPath === 'default' || this.settings.quartzPath.trim() === "") {
+            new Notice('Error: Missing Quartz Path. Please set one in Quartz Publish Button plugin settings.', 10000);
+            return false;
+        }
 
-	onunload() {
+        const path = normalizePath(this.settings.quartzPath);
+        if (!fs.existsSync(path)) {
+            new Notice('Error: Quartz path does not exist!\nPlease check the path in the Quartz Publish Button plugin settings.', 10000);
+            return false;
+        }
 
-	}
+        return true;
+    }
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    // Function to run Quartz sync
+    runQuartzSync() {
+        if (!this.validateQuartzPath()) return;
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+        new Notice('Publishing with Quartz...');
+
+        exec(`cd ${this.settings.quartzPath} && npx quartz sync`, (error, stdout, stderr) => {
+            if (error) {
+                new Notice(`Execution Error: ${error.message}`, 40000);
+                return;
+            }
+            if (stderr && stderr.toLowerCase().includes("error")) {
+                new Notice(`Error: ${stderr}`, 40000);
+                return;
+            }
+            if (stdout) {
+                new Notice(`${stdout}\n\n(click to dismiss)`, 100000);
+            }
+        });
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+// Settings Tab for configuring the Quartz repository path
+class QuartzSettingTab extends PluginSettingTab {
+    plugin: QuartzPublishButtonPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: QuartzPublishButtonPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    display(): void {
+        const { containerEl } = this;
+        containerEl.empty();
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Quartz Repository Location')
+            .setDesc('The path to the folder that contains the Quartz files.')
+            .addText(text => text
+                .setPlaceholder('Quartz repo path')
+                .setValue(this.plugin.settings.quartzPath)
+                .onChange(async (value) => {
+                    this.plugin.settings.quartzPath = value;
+                    await this.plugin.saveSettings();
+                }));
+    }
 }
